368fc4d960a7a00c9986568de504d4d359033d21
dahyeon1216 <sally1023p@gmail.com>
2025-11-18 20:44:44 +0900

    chore:#17-todo,todoCategory entity 코드 세팅

--- src/main/java/com/req2res/actionarybe/domain/todo/entity/Todo.java
+++ src/main/java/com/req2res/actionarybe/domain/todo/entity/Todo.java
@@ -1,4 +1,44 @@
 package com.req2res.actionarybe.domain.todo.entity;
 
-public class Todo {
+import com.req2res.actionarybe.global.Timestamped;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "todo")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Todo extends Timestamped {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "user_id", nullable = false)
+    private Long userId;   // 두두 주인
+
+    @Column(nullable = false)
+    private String title;  // 두두 제목
+
+    @Column(nullable = false)
+    private LocalDateTime date;   // 목표 날짜
+
+    @Enumerated(EnumType.STRING)
+    @Builder.Default
+    private Status status = Status.PENDING; // 기본값 설정
+
+    @Column(name = "category_id")
+    private Long categoryId; // 카테고리 ID (NULL 가능)
+
+    public enum Status {
+        PENDING,
+        DONE,
+        FAILED
+    }
 }
+
--- src/main/java/com/req2res/actionarybe/domain/todo/entity/TodoCategory.java
+++ src/main/java/com/req2res/actionarybe/domain/todo/entity/TodoCategory.java
@@ -1,4 +1,28 @@
 package com.req2res.actionarybe.domain.todo.entity;
 
-public class TodoCategory {
+import com.req2res.actionarybe.global.Timestamped;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "todo_category")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class TodoCategory extends Timestamped {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;  // 카테고리 id
+
+    @Column(name = "user_id", nullable = false)
+    private Long userId;   // 카테고리 소유 사용자
+
+    @Column(nullable = false)
+    private String name;   // 카테고리 이름
+
+    @Column
+    private String color;
 }
